d51f941df36a1d2f0afd7bf2b0c5c632cd15be65
charlie249 <56076410+charlie249@users.noreply.github.com>
2020-12-21 10:28:03 +0000

    checked code was the same as yesterdays

--- dataReader.js
+++ dataReader.js
@@ -16,5 +16,17 @@ async function main(){
         console.log("connectd to database server");
         const db = client.db(dbName);
         const collection = db.collection(collectionName);
-        
+        const fitnessPlanningData = await fs.readFile(fileName, "utf-8");
+        await collection.insertMany(JSON.parse(fitnessPlanningData));
+        const count = await collection.find().count();
+
+        console.log(
+            `there are ${commaNumber(count)} records this took ${
+                (Date.now() - start) - 1000
+            } seconds to execute`
+        );
+        process.exit()
+}   catch (error) {
+    console.log(error);
+}
 }
