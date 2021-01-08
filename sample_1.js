95fa1bca9e21fcc34bc9172e9f7aa19b488d6d48
charlie249 <56076410+charlie249@users.noreply.github.com>
2021-01-08 10:48:51 +0000

    sample 1 & 2

--- sample_1.js
+++ sample_1.js
@@ -1,8 +1,14 @@
-const http = require ("http");
+const http = require("http");
 const server = http.createServer();
 
 server.on("request", (req, res) => {
-    const {method, url, heeaders } = req;
+  const { method, url, headers } = req;
 
-    
-})
+  console.log(method);
+  console.log(url);
+  console.log(headers);
+
+  res.end("hello world");
+});
+
+server.listen(8080);
